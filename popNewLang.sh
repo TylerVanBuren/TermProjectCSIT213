e807c3a8abe804e978181c568a902c31fb1d53a6
TYLER VAN_BUREN <tyleravb@gmail.com>
2024-04-24 21:27:05 -0400

    Fixed an accidental deletion

--- popNewLang.sh
+++ popNewLang.sh
@@ -4,6 +4,6 @@
 NEW=$1
 
 #Bracket expansion is so cool and I've always wanted to use it in a project
-mkdir -p $NEW/{PhraseBook/,Rules/}
+mkdir -p $NEW/{Dictionary/,PhraseBook/,Rules/}
 touch $NEW/Rules/{Consonants,IllegalClusters,InflectRules,SyllableRules,Vowels}.txt
 
